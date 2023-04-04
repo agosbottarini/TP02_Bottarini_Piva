2000d86d4a9f0d71f3fc14973ef1ca90d88d673c
AGOSTINA BOTTARINI <47866440@ort>
2023-04-04 10:17:28 -0300

    Subiendo archivos

---
+++ Persona.cs
@@ -0,0 +1,42 @@
+class Persona
+{
+    public int DNI{get;set;}
+    public string Apellido{get;private set;}
+    public string Nombre{get;set;}
+     public string Email{get;set;}
+    public DateTime FechaNacimiento{get;set;}
+    private int Edad{get;set;}
+
+    public Persona()
+    {
+
+    }
+    public Persona(int dni, string apellido, string nom, string email, DateTime fn = new DateTime())
+    {
+        DNI = dni;
+        Apellido = apellido;
+        Nombre = nom;
+        Email = email;
+        FechaNacimiento = fn;
+        Edad = 0;
+    }   
+
+    public int ObtenerEdad()
+    {
+
+        DateTime FechaNacimientoHoy = new DateTime(DateTime.Today.Year, FechaNacimiento.Month, FechaNacimiento.Day);
+        if (FechaNacimientoHoy< DateTime.Today)  Edad = DateTime.Today.Year - FechaNacimiento.Year;
+            else   Edad = DateTime.Today.Year - FechaNacimiento.Year -1;
+        return Edad; 
+    }
+
+    public bool PuedeVotar()
+    {
+        bool puede = false;
+        if(Edad >= 16)
+        {
+            puede = true;
+        }
+        return puede;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+Dictionary<int, Persona> dicPersonas = new Dictionary<int, Persona>();
+ 
+void CargarPersona()
+{
+    int dni = Funciones.IngresarEntero("Ingrese DNI ");
+    string apellido = Funciones.IngresarTexto("Ingrese Curso ");
+    string nom = Funciones.IngresarTexto("Ingrese Nombre ");
+    string email = Funciones.IngresarTexto("Ingrese su Email");
+    DateTime fn = Funciones.IngresarFecha("Ingrese Fecha de Nacimiento (aaaa/mm/dd) ");
+    
+    Persona unaPer =  new Persona(dni, apellido, nom, email, fn);
+    dni = Funciones.IngresarEntero("Ingrese DNI ");
+    apellido = Funciones.IngresarTexto("Ingrese Apellido ");
+    nom = Funciones.IngresarTexto("Ingrese Nombre ");
+    email = Funciones.IngresarTexto("Ingrese Email");
+    fn = Funciones.IngresarFecha("Ingrese Fecha de Nacimiento (aaaa/mm/dd) ");
+    
+
+    dicPersonas.Add(dni,unaPer);
+}
